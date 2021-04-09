94908b2262f107e2a7cf1f3ae8563b872bedc5e9
leotabor18 <leotabor13@outlook.com>
2021-04-09 21:38:53 +0800

    changing path resourses

--- Modal/home.php
+++ Modal/home.php
@@ -12,11 +12,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link rel="stylesheet" href="/root/css/stylesheet.css">
+    <link rel="stylesheet" href="../css/stylesheet.css">
  
     <title>PHP CRUD</title>
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-    <script src="/root/Javascript/jquery-3.6.0.js"></script> 
+    <script src="../Javascript/jquery-3.6.0.js"></script> 
   </head>
   <body>
   
@@ -154,7 +154,7 @@
       </div>
     </div>
 
-   <script src="/root/javascript/home.js"></script>
+   <script src="../javascript/home.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   </body>
 </html>
