e1684a5d55af9cf081c850a86318192ca40e98e8
leotabor18 <leotabor13@outlook.com>
2021-04-09 21:50:29 +0800

    changing path resourses

--- Javascript/index.js
+++ Javascript/index.js
@@ -95,7 +95,7 @@ btn_register.addEventListener('click', event =>{
     return;
   }
 
-  form_register.action = './modal/register.php';
+  form_register.action = './Modal/register.php';
   
 });
 
@@ -122,7 +122,7 @@ btn_login.addEventListener('click', event =>{
       event.preventDefault();
       return;
     }
-    form_login.action = './modal/login.php';
+    form_login.action = './Modal/login.php';
 
 });
 //
