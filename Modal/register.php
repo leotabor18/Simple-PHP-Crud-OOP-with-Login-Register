8c5c79463d84aea13c238aedccd035ebd641ce36
leotabor18 <leotabor13@outlook.com>
2021-04-10 11:00:17 +0800

    Debugging datbase connection

--- Modal/register.php
+++ Modal/register.php
@@ -11,14 +11,14 @@
      );
 
      $encoded = json_encode($arr);
-
+     echo $encoded;
     //  $crud = new LoginRegister();
     //  $register = $crud->_register($encoded);
-     if($register){
-        echo "<script>console.log(".$register.")</script>";   
-     } else {
-        echo "<script>console.log('error')</script>";   
-     }
+    //  if($register){
+    //     echo "<script>console.log(".$register.")</script>";   
+    //  } else {
+    //     echo "<script>console.log('error')</script>";   
+    //  }
             
  }
 
